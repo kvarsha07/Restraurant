63fb2c00d1677e8a8d3ffa1867ef059d7257d527
Pooja Karma <pooja.karma95@gmail.com>
2024-02-21 13:48:43 +0530

    add shimmer

---
+++ src/components/Shimmer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Shimmer = () => {
+  return (
+    <div className='shimer-container'>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+      <div className='shimemr-card'></div>
+
+    </div>
+  )
+}
+
+export default Shimmer 
